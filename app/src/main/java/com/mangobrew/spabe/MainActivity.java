2729c87bab9a75e5ff95ec46a52d62fe8babb19d
DerickMacbookAir <derickdev6@gmail.com>
2022-10-04 00:58:56 -0500

    First work day

---
+++ app/src/main/java/com/mangobrew/spabe/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mangobrew.spabe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+    }
+
+    public void doLogin(View v){
+        setContentView(R.layout.start);
+    }
+}
